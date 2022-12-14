e82be7c5cdca8de574f93f070d4548456eca0517
shajib1927 <shajibshahrear@gmail.com>
2022-12-14 23:14:18 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -7,27 +7,10 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
-      {/* <LoadCountries></LoadCountries> */}
       <Header></Header>
       <Countries></Countries>
     </div>
   );
 }
-
-// function LoadCountries() {
-//   const [countries, setCountries] = useState([])
-//   useEffect(() => {
-//     fetch('https://restcountries.com/v3.1/all')
-//     .then(res =>res.json())
-//     .then(data => setCountries(data))
-//   }, [])
-
-//   return (
-//     <div>
-//       <h1>Country Details</h1>
-//       <h3>Total Countries: {countries.length}</h3>
-//     </div>
-//   )
-// }
 
 export default App;
--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useEffect,useState } from 'react';
+import Country from './Country/Country';
 
 const Countries = () => {
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data))
+    },[]);
+
     return (
         <div>
-            <h2>Countries in Other file</h2>
+            <h2>Hello From Countries!</h2>
+            <h3>Latest!</h3>
+            {countries.map(country => <Country name = {country.name.common}></Country>)}
         </div>
     );
 };
