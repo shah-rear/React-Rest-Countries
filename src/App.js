3aa6d03bd35a05a0f65dfcd7c1bd96140e8e8160
shajib1927 <shajibshahrear@gmail.com>
2022-12-14 21:46:13 +0300

    3rd

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+import Countries from './components/Countries/Countries';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+      {/* <LoadCountries></LoadCountries> */}
+      <Header></Header>
+      <Countries></Countries>
     </div>
   );
 }
 
-function LoadCountries() {
-  const [countries, setCountries] = useState([])
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res =>res.json())
-    .then(data => setCountries(data))
-  }, [])
+// function LoadCountries() {
+//   const [countries, setCountries] = useState([])
+//   useEffect(() => {
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res =>res.json())
+//     .then(data => setCountries(data))
+//   }, [])
 
-  return (
-    <div>
-      <h1>Country Details</h1>
-      <h3>Total Countries: {countries.length}</h3>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <h1>Country Details</h1>
+//       <h3>Total Countries: {countries.length}</h3>
+//     </div>
+//   )
+// }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>This is a Header</h2>
+            <nav>
+                <a className='link' href="/home">Home</a>
+                <a className='link' href="/tt">App</a>
+                <a className='link' href="/dd">Web</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
